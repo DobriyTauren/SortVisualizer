f9fab7d9112b20e8b1d79ea91b8933769130a59f
DobriyTauren <dobriytauren@gmail.com>
2024-03-28 16:40:17 +0300

    add some counters, dynamic interface

--- SortVisualizer/SortVisualizer.Client/classes/SortService.cs
+++ SortVisualizer/SortVisualizer.Client/classes/SortService.cs
@@ -3,8 +3,14 @@
     public static event EventHandler StyleChanged;
 
     private static int _delay = 10;
-
+    private static int _arrayAccessCount = 0;
+    private static int _swapCount = 0;
+    private static int _compareCount = 0;
+    
     public static int Delay { get => _delay; set => _delay = value; }
+    public static int SwapCount { get => _swapCount; }
+    public static int ArrayAccessCount { get => _arrayAccessCount; }
+    public static int CompareCount { get => _compareCount; }
 
     public static List<ArrayElement> GenerateArray()
     {
@@ -30,29 +36,45 @@
 
     public static async Task BubbleSort(List<ArrayElement> arrayElements)
     {
+        _arrayAccessCount = 0;
+        _swapCount = 0;
+        _compareCount = 0;
+
         for (int i = 0; i < arrayElements.Count; i++)
         {
             for (int j = 0; j < arrayElements.Count - i - 1; j++)
             {
+                #region color red
                 arrayElements[j].Color = "red";
                 arrayElements[j + 1].Color = "red";
 
                 OnStyleChanged();
 
                 await Task.Delay(_delay);
+                #endregion
+                
                 var value1 = arrayElements[j].Text;
+                _arrayAccessCount++;
+
                 var value2 = arrayElements[j + 1].Text;
+                _arrayAccessCount++;
+
+                _compareCount++;
                 if (value1 > value2)
                 {
                     await Swap(j, j + 1, arrayElements);
+                    
+                    _swapCount++;                    
                 }
 
+                #region color blue
                 arrayElements[j].Color = "blue";
                 arrayElements[j + 1].Color = "blue";
 
                 OnStyleChanged();
 
                 await Task.Delay(_delay);
+                #endregion
             }
             arrayElements[arrayElements.Count - i - 1].Color = "green";
         }
@@ -61,8 +83,14 @@
     private static async Task Swap(int index1, int index2, List<ArrayElement> arrayElements)
     {
         var temp = arrayElements[index1];
+        _arrayAccessCount++;
+
         arrayElements[index1] = arrayElements[index2];
+        _arrayAccessCount++;
+        _arrayAccessCount++;
+
         arrayElements[index2] = temp;
+        _arrayAccessCount++;
 
         OnStyleChanged();
     }
