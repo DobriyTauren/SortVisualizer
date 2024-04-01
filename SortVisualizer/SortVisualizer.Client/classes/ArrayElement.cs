e261f41296730b70d42c4b4d72979ce87a250193
DobriyTauren <dobriytauren@gmail.com>
2024-04-01 12:02:38 +0300

    blank new pages

--- SortVisualizer/SortVisualizer.Client/classes/ArrayElement.cs
+++ SortVisualizer/SortVisualizer.Client/classes/ArrayElement.cs
@@ -7,25 +7,4 @@
     public string Color { get; set; }
 
     public string Width { get; set; }
-
-    //public void ChangeStyle(string attribute, string value)
-    //{
-    //    int attributeIndex = _style.IndexOf(attribute);
-
-    //    if (attributeIndex != -1)
-    //    {
-    //        _style = _style.Remove(attributeIndex, _style.IndexOf(";", attributeIndex) - attributeIndex);
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine($"attribute not found!\nattribute: {attribute}");
-    //    }
-
-    //    _style += $"{attribute}: {value};";
-    //}
-
-    //public void SetStyle(string value)
-    //{
-    //    _style = value;
-    //}
 }
